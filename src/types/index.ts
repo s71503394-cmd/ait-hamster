19e4eac6fb3ac36f2981f5e00aa1f5c14e9389c7
s71503394-cmd <s71503394@gmail.com>
2026-06-07 13:50:24 +0300

    Add TypeScript types

---
+++ src/types/index.ts
@@ -0,0 +1,32 @@
+export interface GameState {
+  coins: number;
+  energy: number;
+  maxEnergy: number;
+  level: number;
+  totalClicks: number;
+  lastEnergyRefillTime: number;
+  upgrades: Upgrade[];
+  passiveIncome: number;
+}
+
+export interface Upgrade {
+  id: string;
+  name: string;
+  description: string;
+  cost: number;
+  level: number;
+  maxLevel: number;
+  type: 'clickPower' | 'energy' | 'passive' | 'energyRegen';
+  value: number;
+  icon: string;
+}
+
+export interface GameConfig {
+  initialCoins: number;
+  initialEnergy: number;
+  maxEnergy: number;
+  energyCostPerClick: number;
+  coinPerClick: number;
+  energyRegenRate: number;
+  energyRegenDelay: number;
+}
